736b5410c9cfd7b0ea72b2781258fbaff42cd020
cnorrisj <c.norrisjones@gmail.com>
2019-10-17 21:51:20 -0700

    auto script updates

--- epautoupdate.sh
+++ epautoupdate.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-echo $discord_url
-cd /mnt/c/Users/Chris\ Norris-Jones/Desktop/Projects/virtenv/discordpodupdate/bin
+cd ~/Projects/virtenvs/discordpodupdate/bin
 .  ./activate
 cd ../../../PodcastEpUpdater
 python3 episodeupdate.py --web $discord_url
